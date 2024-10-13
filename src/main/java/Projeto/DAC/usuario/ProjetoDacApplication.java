0d8db1ef39cc6de63cf39601e2db35052edaf5c5
is-NatashaAguiar <nataaguiar2002@gmail.com>
2024-10-13 07:48:23 -0300

    First commit

---
+++ src/main/java/Projeto/DAC/usuario/ProjetoDacApplication.java
@@ -0,0 +1,13 @@
+package Projeto.DAC.usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoDacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoDacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Projeto/DAC/usuario/ProjetoDacApplicationTests.java
@@ -0,0 +1,13 @@
+package Projeto.DAC.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoDacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
